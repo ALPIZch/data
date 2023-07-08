87901f52f073fdcc476fdf78ab1ecce834700afe
ALPIZch <dominik@alpiz.ch>
2023-07-08 09:57:33 +0100

    Updated static data

--- script.sh
+++ script.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-# Führe die gewünschten Befehle aus
-in2csv data.xls > export.csv
-csvtojson export.csv | jq > data.json
+# Setze die erforderlichen Variablen
+repository="https://github.com/ALPIZch/data.git"
+ordner="/DATA/AppData/scripts/ALPIZ/lehrstellen/public"
+commit_message="Updated static data"
 
-cp data.json files/ALPIZ/582582153275
+
+# Ausführen der Befehle
+in2csv data.xls > export.csv
+csvtojson ./export.csv | jq > data.json
+cp data.json /public/data.json
+
+cd public
+
+git init
+
+# Füge alle Dateien zum Git-Index hinzu
+git add .
+
+# Erstelle einen neuen Commit
+git commit -m "$commit_message"
+
+# Verknüpfe das lokale Repository mit dem GitHub-Repository
+git remote add origin "$repository"
+
+# Pushe den Commit auf das GitHub-Repository
+git push --set-upstream origin main
+
